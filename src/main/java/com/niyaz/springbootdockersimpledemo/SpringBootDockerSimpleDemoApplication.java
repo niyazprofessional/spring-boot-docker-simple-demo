9c775e95f4f183d3051d19a5f24707e3e1740178
Niyaz Professional <niyaz.professional@gmail.com>
2021-11-21 17:37:37 +0530

    Create simple Spring Boot Project using Spring Initializer with web dependency using Java 17

---
+++ src/main/java/com/niyaz/springbootdockersimpledemo/SpringBootDockerSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.niyaz.springbootdockersimpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerSimpleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerSimpleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niyaz/springbootdockersimpledemo/SpringBootDockerSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niyaz.springbootdockersimpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerSimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
